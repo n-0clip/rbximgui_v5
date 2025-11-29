b79a5dd571a9f2ab7ff79d74566dc3ee0db8a4b8
n-0clip <h0c1late@gmail.com>
2025-11-29 11:12:53 +0300

    Update killfeed.lua

--- killfeed.lua
+++ killfeed.lua
@@ -1,28 +1,5 @@
 -- v3
 
-local library = loadstring(game:GetObjects("rbxassetid://7657867786")[1].Source)()
-local Wait = library.subs.Wait
 
-local Players = game:GetService("Players")
-local RunService = game:GetService("RunService")
-local UserInputService = game:GetService("UserInputService")
-local Workspace = game:GetService("Workspace")
-local TeleportService = game:GetService("TeleportService")
-local Lighting = game:GetService("Lighting")
-local VirtualInputManager = game:GetService("VirtualInputManager")
-local Camera = Workspace.CurrentCamera
-local LocalPlayer = Players.LocalPlayer
-
-local SupportedGames = {
-    [301549746] = "Counter Blox",
-    [292439477] = "Phantom Forces",
-    [17625359962] = "Rivals",
-    [286090429] = "Arsenal",
-    [982263812] = "Murder Mystery 2",
-    [3837841034] = "Bad Business"
-}
-
-local PlaceId = game.PlaceId
-local GameName = SupportedGames[PlaceId] or "Universal"
 
 -- next is obfucated bcs of skidders.
